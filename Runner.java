39d17eec87171817b9dd55236aa68f808205d0d7
Olena Borodych <olenaborodych@gmail.com>
2024-01-14 20:00:38 +0200

    fix bug with initializing of 'secret' object

--- Runner.java
+++ Runner.java
@@ -1,16 +1,17 @@
-import java.sql.SQLOutput;
-
 public class Runner {
     public static void main(String[] args) {
+
+        Secret secret;
 
         try {
             String command = args[0];
             String filePath = args[1];
             if (args.length > 2) {
-                String key = args[2];
-            }
+                Integer key = Integer.parseInt(args[2]);
+                secret = new Secret(filePath, key);
+            } else secret = new Secret(filePath);
 
-            Secret secret = new Secret(filePath);
+
             switch (command) {
                 case "ENCRYPT":
                     secret.encrypt();
@@ -24,10 +25,10 @@ public class Runner {
                 default:
                     System.out.println("Input parameter " + command + "is unknown.");
             }
-        }catch(java.lang.IndexOutOfBoundsException e){
+        } catch (java.lang.IndexOutOfBoundsException e) {
             System.out.println("\nSome arguments are missing. Please, follow the instruction:\n\n" +
                     "Run the program with the command 'java -jar myApp.jar command filePath key', where\n\n" +
-                    "'command' is one of 'ENCRYPT', 'DECRYPT', 'BRUTE_FORCE;\n"+
+                    "'command' is one of 'ENCRYPT', 'DECRYPT', 'BRUTE_FORCE;\n" +
                     "'filePath' is the absolute path to the file with the source code;\n" +
                     "'key' is the cypher key. In case of BRUTE_FORCE command the key is omitted.");
         }
