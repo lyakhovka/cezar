3aa770d45f371d373717ce7d59bbcb1ea0e04db8
Olena Borodych <olenaborodych@gmail.com>
2024-01-14 19:44:01 +0200

    add info message for the case when wrong jar is run with wrong paramethers

--- Runner.java
+++ Runner.java
@@ -3,23 +3,33 @@ import java.sql.SQLOutput;
 public class Runner {
     public static void main(String[] args) {
 
-        String command = args[0];
-        String filePath = args[1];
-        if(args.length>2) {String key = args[2];}
+        try {
+            String command = args[0];
+            String filePath = args[1];
+            if (args.length > 2) {
+                String key = args[2];
+            }
 
-        Secret secret = new Secret(filePath);
-        switch (command){
-            case "ENCRYPT":
-                secret.encrypt();
-                break;
-            case "DECRYPT":
-                secret.decrypt();
-                break;
-            case "BRUTE_FORCE":
-                secret.bruteForce();
-                break;
-            default:
-                System.out.println("Input parameter " + command + "is unknown.");
+            Secret secret = new Secret(filePath);
+            switch (command) {
+                case "ENCRYPT":
+                    secret.encrypt();
+                    break;
+                case "DECRYPT":
+                    secret.decrypt();
+                    break;
+                case "BRUTE_FORCE":
+                    secret.bruteForce();
+                    break;
+                default:
+                    System.out.println("Input parameter " + command + "is unknown.");
+            }
+        }catch(java.lang.IndexOutOfBoundsException e){
+            System.out.println("\nSome arguments are missing. Please, follow the instruction:\n\n" +
+                    "Run the program with the command 'java -jar myApp.jar command filePath key', where\n\n" +
+                    "'command' is one of 'ENCRYPT', 'DECRYPT', 'BRUTE_FORCE;\n"+
+                    "'filePath' is the absolute path to the file with the source code;\n" +
+                    "'key' is the cypher key. In case of BRUTE_FORCE command the key is omitted.");
         }
     }
 
