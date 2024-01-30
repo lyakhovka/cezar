12f509dca823afc2571c56ca89c6148f04494e0f
Olena Borodych <olenaborodych@gmail.com>
2024-01-30 10:00:37 +0200

    update ERROR_LOG variable; introduce constants for input args[]

--- BeforeRun.java
+++ BeforeRun.java
@@ -3,34 +3,48 @@ import java.nio.file.Path;
 
 public class BeforeRun {
 
-    private static String instruction = "\nSome arguments are missing or not valid. Please, follow the instruction:\n\n" +
-            "Run the program with the command 'java -jar myApp.jar command filePath key', where\n\n" +
-            "'command' is one of 'ENCRYPT', 'DECRYPT', 'BRUTE_FORCE;\n" +
-            "'filePath' is the absolute path to the file with the source code;\n" +
-            "'key' is the cypher key. In case of BRUTE_FORCE command the key is omitted.";
+    private static final String ERROR_LOG = """
+                        
+            Some arguments are missing or not valid. Please, follow the instruction:
+                        
+            Run the program with the command 'java -jar myApp.jar command filePath key', where
+            'command' is one of 'ENCRYPT', 'DECRYPT', 'BRUTE_FORCE';
+            'filePath' is the absolute path to the file with the source code;
+            'key' is the cypher key. In case of BRUTE_FORCE command the key is omitted.""";
 
-    //Validation for input parameters:
+//Validation for input parameters:
 //If amount of input params are less then 2
 //OR any of input params is not valid
 //OR amount of input params are 2 but command is not 'BRUTE_FORCE'
 //then info message is shown and program ends with code 1.
-    private static void validateParams(String args[]) {
+    private static void validateParams(String[] args) {
         int argNumber = args.length;
+        String command = "";
+        String filePath = "";
+        String key = "";
 
-        if ((argNumber < 2 || !validateCommand(args[0]) || (!validatePath(args[1]))) ||
-                (argNumber == 2 && !args[0].equals("BRUTE_FORCE")) ||
-                (argNumber > 2 && !validateKey(args[2]))) {
+        if (argNumber < 2) {
+            showInfoAndQuit();
+        } else {
+            command = args[0];
+            filePath = args[1];
+        }
+        if (argNumber > 2) key = args[2];
+
+        if ((argNumber < 2 || !validateCommand(command) || (!validatePath(filePath))) ||
+                (argNumber == 2 && !command.equals("BRUTE_FORCE")) ||
+                (argNumber > 2 && !validateKey(key))) {
             showInfoAndQuit();
         } else System.out.println("Input Parameters are Valid.");
     }
 
-    public static Secret secretInit(String args[]) {
+    public static Secret secretInit(String[] args) {
         validateParams(args);
         return args.length == 2 ? new Secret(args[1]) : new Secret(args[1], Integer.parseInt(args[2]));
     }
 
     private static void showInfoAndQuit() {
-        System.out.println(instruction);
+        System.out.println(ERROR_LOG);
         System.exit(1);
     }
 
