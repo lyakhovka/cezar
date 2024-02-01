0e74704af263ceb5613c4a2134b81b2eee58b657
Olena Borodych <olenaborodych@gmail.com>
2024-02-01 18:33:11 +0200

    introduce variables for input parameters indexes; separate parameters validation and 'sected' object initialization

--- Runner.java
+++ Runner.java
@@ -1,8 +1,13 @@
 public class Runner {
     public static void main(String[] args) {
         int commandParameterIndex = 0;
-        Secret secret = BeforeRun.secretInit(args);
+        int sourceFilePathIndex = 1;
+        int keyIndex = 2;
+
+        BeforeRun.validateParams(args);
         String command = args[commandParameterIndex];
+        Secret secret = args.length == 2 ? new Secret(args[sourceFilePathIndex]) : new Secret(args[sourceFilePathIndex], Integer.parseInt(args[keyIndex]));
+
 
         switch (command) {
             case "ENCRYPT":
